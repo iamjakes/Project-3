4b58f96be71fdcb564dab0546c959679064764c0
DXHASE <danielchas886@gmail.com>
2022-12-19 20:33:05 -0600

    Added Apollo provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Footer from './components/Footer'
 import Header from './components/Header'
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header></Header>
       <section>
@@ -15,6 +25,7 @@ function App() {
       </section>
       <Footer></Footer>
     </div>
+    </ApolloProvider>
   );
 }
 
