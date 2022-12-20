bd137493582bef23425eb5fa6108f13cfee08b65
DXHASE <danielchas886@gmail.com>
2022-12-19 20:27:56 -0600

    Added Apollo provider and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
